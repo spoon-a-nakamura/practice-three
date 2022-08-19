ab1d564f3c0c5efe0dda1473bec33dfa410fd11b
Akifumi Nakamura <a-nakamura@studio-spoon.co.jp>
2022-08-19 20:25:28 +0900

    add line

--- src/home/TCanvas.ts
+++ src/home/TCanvas.ts
@@ -7,6 +7,7 @@ import GUI from 'lil-gui'
 
 export class TCanvas extends TCanvasBase {
   private mesh?: THREE.Mesh
+  private line?: THREE.Line
 
   constructor(parentNode: ParentNode) {
     super(parentNode)
@@ -16,13 +17,14 @@ export class TCanvas extends TCanvasBase {
   }
 
   private setScene = () => {
-    this.scene.background = new THREE.Color('#000')
+    this.scene.background = new THREE.Color('#fff')
     this.camera.position.z = 2
 
     this.setOrbitControls()
   }
 
   private createModel = async () => {
+    // texture
     const loader = new THREE.TextureLoader()
     const texture1 = await loader.loadAsync(publicPath('/resources/spoon1.jpg'))
     const texture2 = await loader.loadAsync(publicPath('/resources/spoon2.jpg'))
@@ -40,7 +42,32 @@ export class TCanvas extends TCanvasBase {
     })
 
     this.mesh = new THREE.Mesh(geometry, material)
+
+    // line
+    const points = []
+    points.push(new THREE.Vector3(-1, 0, 0))
+    points.push(new THREE.Vector3(0, 1, 0))
+    points.push(new THREE.Vector3(1, 0, 0))
+    const lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
+    const lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff })
+    this.line = new THREE.Line(lineGeometry, lineMaterial)
+
+    // text
+    const text3d = new THREE.ExtrudeGeometry('text', {
+      size: 80,
+      height: 5,
+      curveSegments: 12,
+      bevelEnabled: true,
+      bevelThickness: 10,
+      bevelSize: 8,
+      bevelOffset: 0,
+      bevelSegments: 5,
+    })
+
+    // scene
     this.scene.add(this.mesh)
+    this.scene.add(this.line)
+    this.scene.add(text3d)
 
     const gui = new GUI()
     gui.add(material.uniforms.u_progress, 'value', 0, 1, 0.01)
